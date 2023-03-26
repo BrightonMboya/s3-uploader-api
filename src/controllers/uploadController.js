b3d4e576c83ce6e2e0570370785139d990ac1e4f
BrightonMboya <b.mboya@alustudent.com>
2023-03-26 22:43:39 +0200

    configured the return json file

--- src/controllers/uploadController.js
+++ src/controllers/uploadController.js
@@ -2,11 +2,13 @@ const db = require("../database/models/");
 const dotenv = require("dotenv");
 const aws = require("aws-sdk");
 const { randomUUID } = require("crypto");
+const S3 = require("aws-sdk/clients/s3");
+// import S3 from "aws-sdk/clients/s3";
 dotenv.config();
 
 const { File } = db;
 
-const s3 = new aws.S3({
+const s3 = new S3({
   accessKeyId: process.env.ACCESS_KEY,
   secretAccessKey: process.env.SECRET_KEY,
   region: process.env.REGION,
@@ -16,7 +18,10 @@ const s3 = new aws.S3({
 class uploadController {
   //method to upload file and insert in the DB
   static async uploadMyFile(req, res) {
-    if (!req.file) return res.send("Please upload a file, hellloooo");
+    console.log("hello", req.file);
+    console.log("Body of the file", req.file["mimetype"]);
+
+    if (!req.file) return res.send("Please upload a file");
 
     if (req.method !== "POST") {
       return res
@@ -28,19 +33,35 @@ class uploadController {
 
     try {
       //Upload file to S3
-      let { name, type } = req.body;
+      // let { name, type } = req.file["mimetype"];
 
       // remember to test this part
-      const extension = type.splt("/")[1];
+      const extension = req.file["mimetype"].split("/")[1];
 
       const fileParams = {
-        Bucket: process.env.AWS_BUCKET_NAME,
+        Bucket: process.env.BUCKET_NAME,
         Key: `${uniqueName}.${extension}`,
-        ContentType: type,
+        // Key: req.file["originalname"].trim(),
+        ContentType: "image/png",
         Expires: 600,
       };
       const url = await s3.getSignedUrlPromise("putObject", fileParams);
-      res.status(200).json({ url, key: `${uniqueName}.${extension}` });
+
+      // res.status(200).json({ url, key: req.file["originalname"].trim() });
+      res.status(200).json({
+        s3: {
+          ETag: "",
+          Location: `https://${process.env.BUCKET_NAME}.s3.amazonaws.com/${uniqueName}.${extension}`,
+          key: `${uniqueName}.${extension}`,
+          Bucket: process.env.BUCKET_NAME,
+        },
+        db: {
+          createdAt: { val: "NOW()" },
+          id: 1,
+          fileName: `${uniqueName}.${extension}`,
+          fileLink: `https://${process.env.BUCKET_NAME}.s3.amazonaws.com/${uniqueName}.${extension}`,
+        },
+      });
 
       //Insert file name and link in DB
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,17 @@
-const express = require('express');
-const dotenv  = require('dotenv');
+const express = require("express");
+const dotenv = require("dotenv");
+const bodyParser = require("body-parser");
+const multer = require("multer");
+
 const router = express.Router();
 dotenv.config();
-const uploadController = require( '../controllers/uploadController');
+const uploadController = require("../controllers/uploadController");
+// const upload = multer({ dest: "src/uploads/" });
+const upload = multer();
 
-
-  router.get('/', (_, res) => res.send('Welcome to S3 File Uploader'));
-  router.post('/upload', uploadController.uploadMyFile);
-  router.get('/files/',uploadController.getFiles);
+router.get("/", (_, res) => res.send("Welcome to S3 File Uploader"));
+router.post("/upload", upload.single("File"), uploadController.uploadMyFile);
+router.put("/upload", upload.single("File"), uploadController.uploadMyFile);
+router.get("/files/", uploadController.getFiles);
 
 module.exports = router;
