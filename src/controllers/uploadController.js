f33481ce53a28eea6d6b834b6e921089f4ae7b78
BrightonMboya <b.mboya@alustudent.com>
2023-03-26 12:48:24 +0200

    Initial file upload

--- src/controllers/uploadController.js
+++ src/controllers/uploadController.js
@@ -1,44 +1,62 @@
-const db   = require('../database/models/');
-const dotenv  = require('dotenv');
-const aws   = require('aws-sdk');
+const db = require("../database/models/");
+const dotenv = require("dotenv");
+const aws = require("aws-sdk");
+const { randomUUID } = require("crypto");
 dotenv.config();
 
 const { File } = db;
 
 const s3 = new aws.S3({
-  accessKeyId: process.env.AWS_ACCESS_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+  accessKeyId: process.env.ACCESS_KEY,
+  secretAccessKey: process.env.SECRET_KEY,
+  region: process.env.REGION,
+  signatureVersion: "v4",
 });
 
 class uploadController {
-
   //method to upload file and insert in the DB
-  static async uploadMyFile(req, res){
+  static async uploadMyFile(req, res) {
+    if (!req.file) return res.send("Please upload a file");
 
-    if (!req.file)
-      return res.send('Please upload a file')
+    if (req.method !== "POST") {
+      return res
+        .status("405")
+        .json({ errors: { error: "Method not allowed" } });
+    }
+
+    const uniqueName = randomUUID();
 
     try {
-
       //Upload file to S3
+      let { name, type } = req.body;
 
+      // remember to test this part
+      const extension = type.splt("/")[1];
+
+      const fileParams = {
+        Bucket: process.env.AWS_BUCKET_NAME,
+        Key: `${uniqueName}.${extension}`,
+        ContentType: type,
+        Expires: 600,
+      };
+      const url = await s3.getSignedUrlPromise("putObject", fileParams);
+      res.status(200).json({ url, key: `${uniqueName}.${extension}` });
 
       //Insert file name and link in DB
 
       // Return error of success msg
-      
-      } catch (error) {
-        console.log('ERROR', error);
-        return res.status('500').json({ errors: { error: 'Files not found', err: error.message } });
-      }
+    } catch (error) {
+      console.log("ERROR", error);
+      return res.status("500").json({
+        errors: { error: "Failure to upload a file", err: error.message },
+      });
+    }
   }
 
   //method to return files in the DB
   static async getFiles(req, res) {
-    
-     //Code to get all files from DB and return them
-
-   }
+    //Code to get all files from DB and return them
+  }
 }
 
 module.exports = uploadController;
